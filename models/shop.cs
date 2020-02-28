d47effc07c986099dd4b6037084b6a5952d30c18
Student <example@example.com>
2020-02-28 09:18:37 -0800

    redo shop

--- models/shop.cs
+++ models/shop.cs
@@ -1,11 +1,4 @@
 using System;
-namespace Models;
-{
-  public class Bread
-  {
-    public string Type { get; set; }
-    public int Price { get; set; }
+using Models;
 
-    public
-  }
-}
+namespace Shop.Models
--- program.cs
+++ program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Models;
+using Shop.Models;
 
 namespace Shop
 {
@@ -8,7 +8,8 @@ namespace Shop
   {
     static void Main()
     {
-      
+      Console.WriteLine("Welcome to the Bakery! Would you like some bread, a pastry or both?");
+      string userResponse = Console.ReadLine().ToLower();
     }
   }
 }
