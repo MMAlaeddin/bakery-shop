f8267b2293327cec44401bdb3198570bc9033153
Student <example@example.com>
2020-02-28 09:55:55 -0800

    set up bread class

--- models/shop.cs
+++ models/shop.cs
@@ -1,4 +1,28 @@
 using System;
-using Models;
+using System.Collections.Generic;
 
 namespace Shop.Models
+{
+    public class Bread
+  {
+    public int Amount { get; set; }
+    public int Price { get; set; }
+
+    public Bread(int amount, int price)
+    {
+      Amount = amount;
+      Price = price;   
+    }
+    public int GetAmount(int amount)
+    {
+      if (amount == 1)
+      {
+        return 5;
+      }
+        else if (amount == 2 || amount == 3)
+          {
+            return 10;
+          }
+        }
+      }
+    }
